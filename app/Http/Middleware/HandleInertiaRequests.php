7e805f9e53565115ac1c5df6812836c54f55edfa
Dennis Ego <dennis.ego@artcoresociety.com>
2023-11-03 15:07:06 +0100

    add setup for translations

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Game;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
-use Tightenco\Ziggy\Ziggy;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -18,7 +19,7 @@ class HandleInertiaRequests extends Middleware
     /**
      * Determine the current asset version.
      */
-    public function version(Request $request): string|null
+    public function version(Request $request): ?string
     {
         return parent::version($request);
     }
@@ -30,12 +31,66 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return array_merge(parent::share($request), [
-            'ziggy' => function () use ($request) {
-                return array_merge((new Ziggy)->toArray(), [
-                    'location' => $request->url(),
-                ]);
-            },
+        //Get the shared data
+        $data = array_merge(parent::share($request), [
+            //General setup
+            'locales' => fn () => $this->getLocales(),
+            'currentRouteName' => $request->route()->getName(),
+            'flash' => [
+                'success' => fn() => $request->session()->get('success'),
+                'error' => fn() => $request->session()->get('error'),
+            ],
+            'policies' => fn() => $this->getPolicies(),
         ]);
+
+        //Only on the initial load
+        if (!$request->header('X-Inertia')) {
+            $data['app_name'] = config('app.name');
+            $data['translations'] = $this->getTranslations();
+        }
+
+        return $data;
+    }
+
+    protected function getTranslations(): array
+    {
+        //Get the locale settings
+        $locale = config('app.locale');
+        $fallbackLocale = config('app.fallback_locale');
+
+        //Get the files that should be shared with the SPA
+        $files = [
+            'spa',
+        ];
+
+        //Load the translations of the current locale
+        foreach ($files as $file) {
+            $translations[$locale][$file] = trans($file);
+        }
+
+        //Add the fallback translations when required
+        if ($locale !== $fallbackLocale) {
+            foreach ($files as $file) {
+                $translations[$fallbackLocale][$file] = trans($file, [], $fallbackLocale);
+            }
+        }
+
+        return $translations;
+    }
+
+    private function getLocales(): array
+    {
+        return [
+            'currentLocale' => config('app.locale')
+        ];
+    }
+
+    private function getPolicies()
+    {
+        return [
+            'can' => [
+                'manageUsers' => auth()->check() ? auth()->user()->can('manage-users', User::class) : false,
+            ]
+        ];
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,28 @@
 import './bootstrap';
-import '../css/app.css';
+import '../scss/app.scss';
 
 import { createApp, h } from 'vue';
+import { createI18n } from 'vue-i18n'
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+//Setup Vue i18n translations
+const initialPage = JSON.parse(app.dataset.page);
+const i18n = createI18n({
+    locale: initialPage.props.locales.currentLocale,
+    messages: initialPage.props.translations,
+    legacy: false,
+    warnHtmlMessage: false
+});
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue, Ziggy)
+            .use(ZiggyVue)
+            .use(i18n)
             .mount(el);
     },
     progress: {
